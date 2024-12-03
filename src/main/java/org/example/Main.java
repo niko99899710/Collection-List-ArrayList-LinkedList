396a8b830e1b3ac51c1bfd402bc1733f700ffc80
karinaid <bagdasarova-k@inbox.ru>
2024-12-03 21:43:11 +0400

    H/W 1/2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       List<String> color = new ArrayList<>();
+       color.add("green");
+       color.add("blue");
+       color.add("yellow");
+       color.add("red");
+       boolean findColor = color.contains("red");
+       System.out.println(findColor);
+
+       List <Integer> number = new LinkedList<>();
+       number.add(1);
+       number.add(2);
+       number.add(3);
+       number.add(4);
+       number.add(5);
+       number.set(0,5);
+       number.set(4,1);
+       System.out.println(number);
     }
 }
